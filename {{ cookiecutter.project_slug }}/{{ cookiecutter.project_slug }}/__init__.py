f40dc6dd100516621db07ddc89c54d0bf61ed40d
fx_kirin <fx.kirin@gmail.com>
2021-06-21 14:53:05 +0900

    fix typo

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/__init__.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/__init__.py
@@ -1,4 +1,4 @@
 from ._version import __version__
 
-from . import rust_binidng
-from .rust_binidng import log_config_from_project_root, log_config_file
+from . import rust_binding
+from .rust_binding import log_config_from_project_root, log_config_file
